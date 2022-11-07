8d43e124282f5b643ff027da74d9a7f7c6836656
Basil <baisheng@gmail.com>
2022-11-07 13:19:45 +0800

    style(other):

--- scripts/publish-to-npm.sh
+++ scripts/publish-to-npm.sh
@@ -8,11 +8,11 @@ NPM = "https://registry.npmjs.org/"
 echo "Publishing to NPM @ https://registry.npmjs.org/"
 #cd ../apps/jy-server && npm publish -reg $NPM
 
-cd ../packages/asset-server-plugin && pnpm publish -reg $NPM &&\
-cd ../common && pnpm changeset publish -reg $NPM &&\
-cd ../core && pnpm changeset publish -reg $NPM &&\
-cd ../create && pnpm changeset publish -reg $NPM &&\
-cd ../auth && pnpm changeset publish -reg $NPM &&\
-cd ../ali-sms-plugin && pnpm changeset publish -reg $NPM && \
-cd ../wechat-plugin && pnpm changeset publish -reg $NPM
+cd ../packages/asset-server-plugin && pnpm publish --access public $NPM &&\
+cd ../common && pnpm  publish --access public $NPM &&\
+cd ../core && pnpm  publish --access public $NPM &&\
+cd ../create && pnpm  publish --access public $NPM &&\
+cd ../auth && pnpm  publish --access public $NPM &&\
+cd ../ali-sms-plugin && pnpm publish --access public $NPM && \
+cd ../wechat-plugin && pnpm  publish --access public $NPM
 #cd ../editorjs-parser && npm publish -reg $VERDACCIO
