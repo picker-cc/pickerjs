2cf85f80fb6dd1c6273eeeff2e584280b1dc2549
Basil <baisheng@gmail.com>
2022-11-12 22:34:06 +0800

    style(other):

--- scripts/publish-to-npm.sh
+++ scripts/publish-to-npm.sh
@@ -9,8 +9,5 @@ echo "Publishing to NPM @ https://registry.npmjs.org/"
 #cd ../apps/jy-server && npm publish -reg $NPM
 
 #cd ../packages/asset-server-plugin && pnpm publish --access public $NPM &&\
-cd ../packages/create && pnpm publish --access public $NPM &&\
-cd ../auth && pnpm  publish --access public $NPM &&\
-cd ../ali-sms-plugin && pnpm publish --access public $NPM && \
-cd ../wechat-plugin && pnpm  publish --access public $NPM
+cd ../packages/wechat-plugin && pnpm publish --access public $NPM
 #cd ../editorjs-parser && npm publish -reg $VERDACCIO
